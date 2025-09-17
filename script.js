ec1c4b682c17cda60be30447c41e8faba26073a4
Mettel07 <mateushoffmann0707@gmail.com>
2025-09-17 19:07:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+//https://viacep.com.br/ws/01001000/json/
+
+const buttonAdd = document.querySelector(".button-add");
+const inputCep = document.querySelector(".input-cep");
+const listUl = document.querySelector(".list-ul");
+
+const handleClickAdd = async () => {
+  try {
+    if (inputCep.value !== "") {
+      let response = await fetch(
+        `https://viacep.com.br/ws/${inputCep.value}/json`
+      );
+      let json = await response.json();
+      if (json.cep === undefined) {
+        throw new Error("");
+      }
+      const address = json.logradouro !== "" ? json.logradouro : `não definida`;
+      const newLi = document.createElement("li");
+      newLi.innerHTML = `O CEP ${json.cep} é do estado ${json.estado}, cidade ${json.localidade} e sua rua é ${address}.`;
+      newLi.classList.add("newLi");
+      listUl.append(newLi);
+      inputCep.value = "";
+    }
+  } catch (error) {
+    alert("Erro ao buscar CEP! Erro nos dados...");
+  }
+};
+
+const handleEnterAdd = async (e) => {
+  try {
+    if (inputCep.value !== "" && e.key === "Enter") {
+      let response = await fetch(
+        `https://viacep.com.br/ws/${inputCep.value}/json`
+      );
+      let json = await response.json();
+      if (json.cep === undefined) {
+        throw new Error("");
+      }
+      const address = json.logradouro !== "" ? json.logradouro : `não definida`;
+      const newLi = document.createElement("li");
+      newLi.innerHTML = `O CEP ${json.cep} é do estado ${json.estado}, cidade ${json.localidade} e sua rua é ${address}.`;
+      newLi.classList.add("newLi");
+      listUl.append(newLi);
+      inputCep.value = "";
+    }
+  } catch (error) {
+    alert("Erro ao buscar CEP! Erro nos dados...");
+  }
+};
+buttonAdd.addEventListener("click", handleClickAdd);
+inputCep.addEventListener("keyup", handleEnterAdd);
